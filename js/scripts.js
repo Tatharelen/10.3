834844fd72c4e1ae302dee458787a1a55396ca4e
Michał Sikorski <sikorhurricane@gmail.com>
2018-03-15 11:31:51 +0100

    Advance project in progress

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,35 @@ $(function(){
 		carouselList.css({marginLeft:0});
 	};
 
-	setInterval(function(){
+	var sliderMove = setInterval(function(){
 		carouselList.animate({'margin-left': '-=400px'}, 1000, moveFirstSlide);
-	}, 2000);
+	}, 3000);
+
+	// Dla ambitnych w trakcie produkcji //
+
+	/*
+
+	var prevSlide = $("#prevSlide");
+		nextSlide = $("#nextSlide");
+
+	prevSlide.click(function(){
+ 		    	
+  	});
+
+  	nextSlide.click(function(){
+		carouselList.animate({'margin-left': '-=400px'});
+  	});
+
+  	nextSlide.mouseout(function(){
+  		sliderMove;
+  	})
+
+  	*/
+
+  	// Kiedy naciskam nextSlide slider wyraźnie przyspiesza, ale chciałem miec efekt, że po wsicnieciu,
+  	// slide szybko się zmienia a po zjechaniu "mouseout" wszystko wraca do normy. Jednak zauważam przeskok, że
+  	// przed powrotem animacja wraca do jakiegos poprzedniego elementu i dopiero rozpoczyna sekwencje. 
+
+
 
 });
